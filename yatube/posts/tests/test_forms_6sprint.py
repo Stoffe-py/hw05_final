2fda6bb7489062dd1047cddbd1382d813be765e6
Григорий <g.xoxlof@bk.ru>
2021-05-20 23:53:06 +0700

    1.2 rewiev

--- yatube/posts/tests/test_forms_6sprint.py
+++ yatube/posts/tests/test_forms_6sprint.py
@@ -154,6 +154,8 @@ class AddCommentFormTest(TestCase):
         )
         self.assertRedirects(response, reverse('post',
                                                kwargs={'username': 'TestName',
-                                                       'post_id': self.post.id}))
+                                                       'post_id':
+                                                        self.post.id}))
         self.assertEqual(Comment.objects.count(), self.comments_count + 1)
-        self.assertTrue(Comment.objects.filter(text='Тестовый комментарий').exists())
+        self.assertTrue(Comment.objects.filter
+                       (text='Тестовый комментарий').exists())
